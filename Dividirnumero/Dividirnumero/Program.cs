91d37c2628282c97f7a4d41ce3bd0fc23fe1c9ef
Kooflish <kgonzalo9812@gmail.com>
2025-05-19 14:21:26 -0400

    .....

---
+++ Dividirnumero/Dividirnumero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ProgramaDivision
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Introduce el primer número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            if (numero2 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero.");
+            }
+            else
+            {
+                double resultado = numero1 / numero2;
+                Console.WriteLine("Resultado de la división: " + resultado);
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Debes introducir números válidos.");
+        }
+    }
+}
