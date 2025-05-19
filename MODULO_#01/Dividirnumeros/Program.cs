1197fa2e7c242fb8ca3516e9b3510238ebb7d7b0
Kooflish <kgonzalo9812@gmail.com>
2025-05-19 16:09:44 -0400

    AGREGA UN COMENTARIO

---
+++ MODULO_#01/Dividirnumeros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dividirnumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //La siguiente linea escribe en la consola
+            Console.WriteLine("Hello Repo");
+        }
+    }
+}
