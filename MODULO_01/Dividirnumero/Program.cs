de0c6a8e6ae624513f1d60ce4f5bca43d8aec21a
Kooflish <kgonzalo9812@gmail.com>
2025-05-19 15:16:07 -0400

    nUEVO ELEMENTO

---
+++ MODULO_01/Dividirnumero/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MiApp // Puedes poner el nombre de tu espacio de nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string continuar = "s";
+
+            while (continuar.ToLower() == "s")
+            {
+                try
+                {
+                    Console.Write("Introduce el primer número: ");
+                    double numero1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Introduce el segundo número: ");
+                    double numero2 = Convert.ToDouble(Console.ReadLine());
+
+                    if (numero2 == 0)
+                    {
+                        Console.WriteLine("Error: No se puede dividir entre cero.");
+                    }
+                    else
+                    {
+                        double resultado = numero1 / numero2;
+                        Console.WriteLine($"Resultado: {resultado}");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Entrada no válida. Debes introducir números.");
+                }
+
+                Console.Write("\n¿Deseas hacer otra división? (s/n): ");
+                continuar = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nPrograma finalizado. ¡Hasta luego!");
+        }
+    }
+}
